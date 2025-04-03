d26071b155f3ba2251972793216edab3d7bdcb96
aaaa <aaaa>
2025-04-03 20:32:58 -0300

    primer commit

---
+++ Practica 1/hola.c
@@ -0,0 +1,25 @@
+#include <stdio.h> // Incluye la biblioteca stdio.h
+#include <stdint.h>
+/*
+int main() { // main es el punto de entrada
+  
+  float fl = 0.1;
+  double db = 0.1;
+  printf("Float: %f Double: %.20f", fl, db);
+
+  return 0; // devuelve un 0 al SO
+}
+*/
+
+/*
+int main(){
+  uint32_t A = 0x120;
+  uint32_t B = 0xFFFFFFFF;
+  uint32_t masc = 0xE0000000;
+
+  if((A & masc) == (B & masc)){
+    printf("Hola, si");
+  } else{
+    printf("Hola, no");
+  }
+}*/
